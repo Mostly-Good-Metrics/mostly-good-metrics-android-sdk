857f96731f8bf0e907951b6ed0492265410a3ddb
Josh Holtz <me@joshholtz.com>
2026-08-03 21:09:37 -0500

    Bump version to 0.4.0

--- mostlygoodmetrics/build.gradle.kts
+++ mostlygoodmetrics/build.gradle.kts
@@ -101,7 +101,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.mostlygoodmetrics"
             artifactId = "sdk"
-            version = "0.3.0"
+            version = "0.4.0"
 
             afterEvaluate {
                 from(components["release"])
